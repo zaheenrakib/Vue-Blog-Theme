34168a801b59f1ee850b39ad8315e6fb0fd5ccbb
zaheenrakib <rakibzaheen@gmail.com>
2025-11-05 19:14:31 +0600

    About Page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,11 +2,13 @@
 import BlogList from '@/views/BlogList.vue';
 import HeroBanner from '@/components/HeroSection/HeroBanner.vue';
 import NewsLetter from '@/components/NewsLetter/NewsLetter.vue';
+import AboutView from './AboutView.vue';
 </script>
 
 <template>
   <main>
     <HeroBanner />
     <BlogList />
+    <AboutView />
   </main>
 </template>
